e66affe60ff865ea76238aafda33713a560f51ad
Muthoni Njoki <fmuthoninjoki@gmail.com>
2024-01-05 23:42:13 +0300

    Added a function that deletes a todo

--- src/TodoList.js
+++ src/TodoList.js
@@ -17,12 +17,16 @@ const TodoList = () => {
   const toggleComplete = id => {
     setTodos(todos.map(todo => todo.id === id ? {todo, completed: !todo.completed} : todo))
   }
+
+  const deleteTodo = id => {
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
   return (
     <div className='TodoList'>
         <h1>Get Things done!</h1>
         <AddTodoForm addTodo={addTodo}></AddTodoForm>
         {todos.map((todo, index) => (
-          <Todo task={todo} key={index} toggleComplete={toggleComplete}/>
+          <Todo task={todo} key={index} toggleComplete={toggleComplete} deleteTodo={deleteTodo} />
         ))}
         <Todo></Todo>
     </div>
